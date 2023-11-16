3cdb236cc8b38e6c720b903ce2ae213a42be71a5
ManhToan2612 <147154654+ManhToan2612@users.noreply.github.com>
2023-11-17 00:02:19 +0700

    Toan

--- app/src/main/java/com/example/du_an_1/Database/DbHelper.java
+++ app/src/main/java/com/example/du_an_1/Database/DbHelper.java
@@ -8,7 +8,7 @@ import androidx.annotation.Nullable;
 
 public class DbHelper extends SQLiteOpenHelper {
     private static final String DB_NAME = "QLG";
-    private static final int DbVersion = 3;
+    private static final int DbVersion = 16;
 
     public DbHelper(@Nullable Context context) {
         super(context, DB_NAME,null, DbVersion);
@@ -23,56 +23,52 @@ public class DbHelper extends SQLiteOpenHelper {
                 "matKhau TEXT NOT NULL)");
         db.execSQL(tb_Admin);
 
+
         // Bang loai giay
         String tb_LoaiGiay= "CREATE TABLE LoaiGiay(" +
-                "maLoai TEXT PRIMARY KEY AUTOINCREMENT," +
+                "maLoai TEXT PRIMARY KEY," +
                 "tenLoai TEXT NOT NULL)";
         db.execSQL(tb_LoaiGiay);
 
         // Bang giay
         String tb_Giay= "CREATE TABLE Giay(" +
-                "maGiay TEXT PRIMARY KEY AUTOINCREMENT," +
+                "maGiay TEXT PRIMARY KEY," +
                 "tenGiay TEXT NOT NULL," +
-                "size INTEGER NOT NULL," +
                 "giaTien INTEGER NOT NULL," +
                 "maLoai TEXT REFERENCES LoaiGiay(maLoai))";
         db.execSQL(tb_Giay);
 
-        // Bang nhan vien
-        String tb_NhanVien= "CREATE TABLE NhanVIen(" +
-                "maNV TEXT PRIMARY KEY AUTOINCREMENT," +
-                "hoTenNV TEXT NOT NULL," +
-                "namSinh TEXT NOT NULL," +
-                "CCCD TEXT NOT NULL)";
-        db.execSQL(tb_NhanVien);
-
         // Bang hoa don
         String tb_HoaDon= "CREATE TABLE HoaDon(" +
-                "maHD TEXT PRIMARY KEY AUTOINCREMENT," +
+                "maHD TEXT PRIMARY KEY," +
                 "maGiay TEXT REFERENCES Giay(maGiay)," +
                 "tenGiay TEXT REFERENCES Giay(tenGiay)," +
-                "soLuong TEXT NOT NULL," +
+                "soLuong INTEGER NOT NULL," +
                 "giaTien INTEGER NOT NULL," +
-                "ngaY TEXT NOT NULL)";
+                "ngay TEXT NOT NULL)";
         db.execSQL(tb_HoaDon);
 
-
         //insert date
-        db.execSQL("INSERT INTO Admin VALUES ('admin','Nguyễn Văn Admin','admin'), " +
-                "('admin1','Nguyen Văn A ','123456')");
+        db.execSQL("INSERT INTO Admin VALUES ('admin1','Nguyễn Văn Admin','admin'), " +
+                "('khachhang1','Nguyen Văn A ','123456')");
 
+        db.execSQL("INSERT INTO LoaiGiay VALUES ('1', 'xxx')," +
+                "('2', 'lll')");
 
+        db.execSQL("INSERT INTO Giay VALUES ('1', 'Giay bong da', '30000', '1')," +
+                "('2', 'Giay', '40000', '2')");
 
+        db.execSQL("INSERT INTO HoaDon VALUES('1', '1', 'Giay bong da', 2, 1231413, '23')," +
+                "('2', '2', 'Giay', 1, 231221, '12')");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         if(oldVersion != newVersion){
-            db.execSQL("DROP TABLE IF EXISTS Admin");
-            db.execSQL("DROP TABLE IF EXISTS LoaiGiay");
-            db.execSQL("DROP TABLE IF EXISTS Giay");
-            db.execSQL("DROP TABLE IF EXISTS HoaDon");
-            db.execSQL("DROP TABLE IF EXISTS NhanVien");
+            db.execSQL("drop table if exists Admin");
+            db.execSQL("drop table if exists HoaDon");
+            db.execSQL("drop table if exists Giay");
+            db.execSQL("drop table if exists LoaiGiay");
 
             onCreate(db);
         }
